8984e72e1f77a1d5a84d43371da790bf0ba3aa29
olisemiracle <olisemiracle904@gmail.com>
2023-02-22 00:04:51 +0100

    Day 1 files

---
+++ 1_debugging.py
@@ -0,0 +1,4 @@
+print("Day 1 - string manipulation")
+print('String concatenation is done with the "+" sign')
+print('e.g. print("Hello " + "World")')
+print("Newlines should be created with a backslash and n.")

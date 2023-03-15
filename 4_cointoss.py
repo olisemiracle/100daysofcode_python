d65112eb2c9419eda9c8993077539d6b20c4cd55
olisemiracle <olisemiracle904@gmail.com>
2023-03-15 12:08:20 +0100

    randomization

---
+++ 4_cointoss.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+ran = random.randint(1, 2)
+if ran == 1:
+    print("Head")
+else:
+    print("Tail")
---
+++ 4_random.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+import my_module
+random_int = random.randint(1, 10)
+random_float = random.random()
+print(random_int)
+print(random_float)
+random_float = random_float * 5
+print(random_float)
+#print(my_module.pi)
